b87895c9632c016cda2bf61c55b3a64dd3d22621
abubakirovxolmirza <abubakirovxolmirza1@gmail.com>
2024-05-17 22:02:36 +0500

    asda

--- certificate/urls.py
+++ certificate/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pdf/<int:pk>', views.PDFUserDetailView.as_view(), name='generate_pdf'),
+    path('pdf/<int:pk>', views.certificate, name='generate_pdf'),
+    path('pdf1', views.pdf, name='pdf')
 ]
--- certificate/views.py
+++ certificate/views.py
@@ -1,50 +1,46 @@
-# import os
-# import json
-# from django.http import HttpResponse, JsonResponse
-# from django.shortcuts import render
-# from django.template.loader import get_template
-# from django.conf import settings
-# from pyhtml2pdf import converter
-# from django.template import TemplateDoesNotExist
-# from django.views.decorators.csrf import csrf_exempt
-# from users.models import User
-# from django.shortcuts import render
-
-# @csrf_exempt
-# def generate_pdf(request):
-#     if request.method == 'POST':
-#         data = json.loads(request.body)
-#         user_id = data.get('user_id')
-#         user = User.objects.filter(id=user_id).first()
-#         user_name = user.ism
-#         user_surname = user.familya
-        
-#         html_template_path = os.path.join(settings.BASE_DIR, 'certificate/templates/certificate', 'pdf.html')
-
-
-      
-#         template = get_template(html_template_path)
-#         html_content = template.render({'ism': user_name, 'familya': user_surname})
-
-
-#         pdf_output_path = os.path.join(settings.BASE_DIR, 'media/certificate', f'{user_name}.pdf')
-#         converter.convert(f'file:///{html_template_path}', f'file/sertificates/{user_name}.pdf')
-        
-        
-#         return render(request, 'certificate/pdf.html', {'ism': user_name, 'familya': user_surname})
-#     else:
-#         return JsonResponse({'error': 'Invalid HTTP method.'}, status=400)
-
-# def pdf(request):
-#     return render(request, 'certificate/pdf.html', {'ism': 'John', 'familya': 'Doe'})
-from easy_pdf.views import PDFTemplateView, PDFTemplateResponseMixin
+import os
+import json
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from django.template.loader import get_template
+from django.conf import settings
+from pyhtml2pdf import converter
+from django.template import TemplateDoesNotExist
+from django.views.decorators.csrf import csrf_exempt
 from users.models import User
-from django.views.generic import DetailView
+from django.shortcuts import render
+
+def certificate(request, pk):
+    try:
+        user = User.objects.filter(id=pk).first()
+        user_name = user.ism
+        user_surname = user.familya
+        template = get_template('certificate/pdf.html')
+        context = {'ism': user_name, 'familya': user_surname}
+        html = template.render(context)
+        return HttpResponse(html)
+    except User.DoesNotExist:
+        return HttpResponse(status=404)
+    
+# from easy_pdf.views import PDFTemplateView, PDFTemplateResponseMixin
+# from users.models import User
+# from django.views.generic import DetailView
+@csrf_exempt
+def pdf(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        user_id = data.get('user_id')
+        user = User.objects.filter(id=user_id).first()
+        user_name = user.ism
+        user_surname = user.familya
+        
+        # return converter.convert(f'http://127.0.0.1:8000/api/pdf/{user_id}', f'{user_name}{user_surname}.pdf')
+    # converter.convert(f'http://127.0.0.1:8000/api/pdf/{user_id}', f'{user_name}{user_surname}.pdf')
+    converter.convert(f'http://127.0.0.1:8000/api/pdf/{user_id}', f'{user_name}{user_surname}.pdf')
+    return JsonResponse({'message': 'Pdf is generated successfully!'})
 
 
-
-
-class PDFUserDetailView(PDFTemplateResponseMixin, DetailView):
-    model = User
-    context_object_name = 'user'
-    template_name = 'certificate/pdf.html'
+# class PDFUserDetailView(PDFTemplateResponseMixin, DetailView):
+#     model = User
+#     context_object_name = 'user'
+#     template_name = 'certificate/pdf.html'
